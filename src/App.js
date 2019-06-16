3d5c395385e8eebcf3130eb74e84115f1574a6db
nawras <nawras.mbc@gmail.com>
2019-06-16 21:23:29 +0400

    working on the rating system

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import { connect } from "react-redux";
 import MovieComponent  from "./components/MovieComponent"
 
 function App(props) {
-  return (
+  console.log(props)
+  const orderList = props.movies_list.sort((a,b)=>(a.rate < b.rate) ? 1 :-1);
+  console.log(orderList);
+  
+   return (
    <div className="App">
       <header>
          <h1>TOP 10  Rating</h1>
       </header>
       <div className="container">
       <button onClick = {props.onRandomClick} >Random Rating</button>{
-      Object.keys(props.movies_list).map(  (movie , index=0)=> {
-       return <MovieComponent movie={props.movies_list[movie]}  key={index++} ></MovieComponent> 
-      })
+        Object.keys(props.movies_list).map(  (movie , index=0)=> {
+          return <MovieComponent movie={orderList[movie]}   key={index++} ></MovieComponent> 
+         })
+      
     } 
       </div>
    </div>
